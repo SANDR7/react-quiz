77577afab39a8bf1a27f2325d8a8b11e8d310582
Sander van Ast <sandervanast1@gmail.com>
2020-10-09 12:59:54 +0200

    Updates

---
+++ precache-manifest.18ace3eb207f1ba5847531c99c3ebee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1ceb0fdd7feec82335a5909113ad5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927a05315d8163d32b27",
+    "url": "/static/css/main.113cf3c4.chunk.css"
+  },
+  {
+    "revision": "91545fef6be1722b26ce",
+    "url": "/static/js/2.e80b9a86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e80b9a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927a05315d8163d32b27",
+    "url": "/static/js/main.28150701.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-quiz/precache-manifest.df219e72c26222e4738fc3c34f5c2cd2.js"
+  "/precache-manifest.18ace3eb207f1ba5847531c99c3ebee2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-quiz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
